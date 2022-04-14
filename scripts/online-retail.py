07d183850f4ba35c387ced7a9c5323f180ed5a67
christianoskata <kira26107@gmail.com>
2022-04-14 17:32:57 -0300

    Adicionando as perguntas de 8-13

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -24,125 +24,233 @@ def typeChange(df, col, type):
 		)
 	return df_new
 
+def treatcsv(df):
+    df_new = df.withColumn(
+        "StockCode",
+        F.when(F.col("StockCode") == "PADS", 0).otherwise(F.col("StockCode"))
+    )
+    df_new = isNA(df_new, 'InvoiceNo')
+    df_new = isNA(df_new, 'StockCode')
+    df_new = isNA(df_new, 'Quantity')
+    df_new = isNA(df_new, 'InvoiceDate')
+    df_new = isNA(df_new, 'UnitPrice')
+    df_new = isNA(df_new, 'CustomerID')
+    df_new = isNA(df_new, 'Country')
+    df_new = df_new.withColumn('UnitPrice', regexp_replace('UnitPrice', ',', '.'))
+    df_new = typeChange(df_new, "UnitPrice", "float")
+    df_new = typeChange(df_new, "Quantity", "int")
+    df_new = (df_new.withColumn("InvoiceDate", F.lpad(F.col('InvoiceDate'), 16, '0'))
+               .withColumn("InvoiceDate",F.to_timestamp(F.col("InvoiceDate"), 'd/M/yyyy HH:mm')))
+    
+    return df_new
+
 def pergunta1(df):
-	df_without_na = isNA(df, "StockCode")
-	df_without_na = isNA(df_without_na, "UnitPrice")
-	df_without_na = isNA(df_without_na, "Quantity")
-	df_without_na = df_without_na.withColumn('UnitPrice', regexp_replace('UnitPrice', ',', '.'))
-	df_final = typeChange(df_without_na, "UnitPrice", "float")
-	df_final = typeChange(df_final, "Quantity", "int")
-	df_final_filtered = df_final.select(
+
+	df_final_filtered = df.select(
 		"StockCode", "Quantity", "UnitPrice"
 		).where(
+			(~F.col('InvoiceNo').rlike("^C")) &
 			(F.col('StockCode').rlike("^gift_0001")) & 
-			(F.col("UnitPrice") > 0)
+			(F.col("UnitPrice") > 0) & 
+			(F.col("Quantity") > 0)
 		)
 	# df_final_filtered.show()
+	print("Pergunta 1")
 	df_final_filtered.groupBy().sum('Quantity').show()
 
 def pergunta2(df):
-	df_without_na = isNA(df, "StockCode")
-	df_without_na = isNA(df_without_na, "UnitPrice")
-	df_without_na = isNA(df_without_na, "Quantity")
-	df_without_na = df_without_na.withColumn('UnitPrice', regexp_replace('UnitPrice', ',', '.'))
-	df_final = typeChange(df_without_na, "UnitPrice", "float")
-	df_final = typeChange(df_final, "Quantity", "int")
-	df_final = (df_final.withColumn("InvoiceDate", F.lpad(F.col('InvoiceDate'), 16, '0'))
-       .withColumn("InvoiceDate",F.to_timestamp(F.col("InvoiceDate"), 'd/M/yyyy HH:mm'))
-      )
-	df_final_filtered = df_final.select(
+	df_final_filtered = df.select(
 		"StockCode", "Quantity", "UnitPrice", "InvoiceDate"
 		).where(
+			(~F.col('InvoiceNo').rlike("^C")) &
 			(F.col('StockCode').rlike("^gift_0001")) & 
-			(F.col("UnitPrice") > 0)
+			(F.col("UnitPrice") > 0) &
+			(F.col("Quantity") > 0)
 		)
-	df_final_filtered.groupBy(F.month(F.col("InvoiceDate")).alias("meses")).count().orderBy('meses').show()
+	print("Pergunta 2")
+	(df_final_filtered.groupBy(F.month(F.col("InvoiceDate"))
+	.alias("meses"))
+	.count()
+	.orderBy('meses', ascending=False)
+	.show())
 	
 def pergunta3(df):
 	df_new = df.select("*").where(F.col("StockCode") == "S")
+	print("Pergunta 3")
 	df_new.groupBy("StockCode").count().show()
 
 def pergunta4(df):
-	df_without_na = isNA(df, "StockCode")
-	df_without_na = isNA(df_without_na, "Quantity")
-	df_final = typeChange(df_without_na, "Quantity", "int")
-	df_new = df_final.select("*").where(~F.col('InvoiceNo').rlike("C"))
-	(df_new.groupBy("StockCode")
+	df_final_filtered = df.select(
+		"*"
+		).where(
+			(~F.col('InvoiceNo').rlike("^C")) &
+			(F.col("StockCode") > 0) &
+			(F.col("Quantity") > 0) &
+			(F.col("UnitPrice") > 0)
+		)
+
+	print("Pergunta 4")
+	(df_final_filtered.groupBy("StockCode")
 	.sum('Quantity')
 	.alias("Total Quantity")
 	.orderBy('Total Quantity.sum(Quantity)', ascending=False)
 	.show())
 
 def pergunta5(df):
-	df_without_na = isNA(df, "StockCode")
-	df_without_na = isNA(df_without_na, "UnitPrice")
-	df_without_na = isNA(df_without_na, "Quantity")
-	df_without_na = df_without_na.withColumn('UnitPrice', regexp_replace('UnitPrice', ',', '.'))
-	df_final = typeChange(df_without_na, "UnitPrice", "float")
-	df_final = typeChange(df_final, "Quantity", "int")
-	df_final = (df_final.withColumn("InvoiceDate", F.lpad(F.col('InvoiceDate'), 16, '0'))
-		.withColumn("InvoiceDate",F.to_timestamp(F.col("InvoiceDate"), 'd/M/yyyy HH:mm'))
-		)
-	df_final_filtered = df_final.select(
+	df_final_filtered = df.select(
 		"StockCode", "Quantity", "UnitPrice", "InvoiceDate"
 			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("StockCode") > 0) &
+				(F.col("Quantity") > 0) &
 				(F.col("UnitPrice") > 0)
 			)
+
+	print("Pergunta 5")
 	(df_final_filtered
 	.groupBy(
 		F.month(F.col("InvoiceDate")).alias("months")
 		).sum('Quantity')
-	.alias("Total Quantity")
-	.orderBy("Total Quantity.sum(Quantity)")
-	.show())
+		.alias("Total Quantity")
+		.orderBy("Total Quantity.sum(Quantity)")
+		.show())
 
 def pergunta6(df):
 
-	df_without_na = isNA(df, "StockCode")
-	df_without_na = isNA(df_without_na, "UnitPrice")
-	df_without_na = isNA(df_without_na, "Quantity")
-	df_without_na = df_without_na.withColumn('UnitPrice', regexp_replace('UnitPrice', ',', '.'))
-	df_final = typeChange(df_without_na, "UnitPrice", "float")
-	df_final = typeChange(df_final, "Quantity", "int")
-	df_final = (df_final.withColumn("InvoiceDate", F.lpad(F.col('InvoiceDate'), 16, '0'))
-		.withColumn("InvoiceDate",F.to_timestamp(F.col("InvoiceDate"), 'd/M/yyyy HH:mm'))
-		)
-	df_final_filtered = df_final.select(
+	df_final_filtered = df.select(
 		"StockCode", "Quantity", "UnitPrice", "InvoiceDate"
 			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("StockCode") > 0) &
+				(F.col("Quantity") > 0) &
 				(F.col("UnitPrice") > 0)
 			)
+	print("Pergunta 6")
 	(df_final_filtered
 	.groupBy(
 		F.hour(F.col("InvoiceDate")).alias("hour")
 		).sum('UnitPrice')
-	.alias("Total UnitPrice")
-	.orderBy("Total UnitPrice.sum(UnitPrice)", ascending=False)
-	.show())
+		.alias("Total UnitPrice")
+		.orderBy("Total UnitPrice.sum(UnitPrice)", ascending=False)
+		.show())
 
 def pergunta7(df):
 
-	df_without_na = isNA(df, "StockCode")
-	df_without_na = isNA(df_without_na, "UnitPrice")
-	df_without_na = isNA(df_without_na, "Quantity")
-	df_without_na = df_without_na.withColumn('UnitPrice', regexp_replace('UnitPrice', ',', '.'))
-	df_final = typeChange(df_without_na, "UnitPrice", "float")
-	df_final = typeChange(df_final, "Quantity", "int")
-	df_final = (df_final.withColumn("InvoiceDate", F.lpad(F.col('InvoiceDate'), 16, '0'))
-		.withColumn("InvoiceDate",F.to_timestamp(F.col("InvoiceDate"), 'd/M/yyyy HH:mm'))
-		)
-	df_final_filtered = df_final.select(
+	df_final_filtered = df.select(
 		"StockCode", "Quantity", "UnitPrice", "InvoiceDate"
 			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("StockCode") > 0) &
+				(F.col("Quantity") > 0) &
 				(F.col("UnitPrice") > 0)
 			)
+	print("Pergunta 7")
 	(df_final_filtered
 	.groupBy(
 		F.month(F.col("InvoiceDate")).alias("months")
 		).sum('UnitPrice')
-	.alias("Total UnitPrice")
-	.orderBy("Total UnitPrice.sum(UnitPrice)", ascending=False)
-	.show())
+		.alias("Total UnitPrice")
+		.orderBy("Total UnitPrice.sum(UnitPrice)", ascending=False)
+		.show())
+
+def pergunta9(df):
+
+	df_final_filtered = df.select(
+		"StockCode", "Quantity", "UnitPrice", "InvoiceDate", "Country"
+			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("StockCode") > 0) &
+				(F.col("Quantity") > 0) &
+				(F.col("UnitPrice") > 0)
+			)
+	print("Pergunta 9")
+	(df_final_filtered
+	.groupBy(
+		F.col('Country').alias("Country")
+		).sum("UnitPrice")
+		.alias("Total UnitPrice")
+		.orderBy("Total UnitPrice.sum(UnitPrice)", ascending=False)
+		.show())
+
+def pergunta10(df):
+	
+	df_final_filtered = df.select(
+		"StockCode", "Quantity", "UnitPrice", "InvoiceDate", "Country"
+			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("UnitPrice") > 0) &
+				(F.col("Quantity") > 0) &
+				(F.col("StockCode").rlike("M"))
+			)
+	print("Pergunta 10")
+	(df_final_filtered
+	.groupBy(
+		F.col('Country').alias("Country")
+		).sum("UnitPrice")
+		.alias("Total UnitPrice")
+		.orderBy("Total UnitPrice.sum(UnitPrice)", ascending=False)
+		.show())
+
+def pergunta11(df):
+	
+	df_final_filtered = df.select(
+		"*"
+			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("UnitPrice") > 0) &
+				(F.col("Quantity") > 0) &
+				(F.col("InvoiceNo") > 0)
+			)
+	df_final_filtered_total_value = df_final_filtered.withColumn(
+    "Total Value",
+    F.ceil(F.expr("Quantity * UnitPrice"))
+    )
+	print("Pergunta 11")
+	(df_final_filtered_total_value
+	.groupBy("InvoiceNo")
+		.sum("Total Valor")
+		.orderBy("sum(Total Valor)", ascending=False)
+		.show())
+
+def pergunta12(df):
+	
+	df_final_filtered = df.select(
+		"*"
+			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("UnitPrice") > 0) &
+				(F.col("Quantity") > 0) &
+				(F.col("InvoiceNo") > 0)
+			)
+	df_final_filtered_total_value = df_final_filtered.withColumn(
+    "Total Value",
+    F.ceil(F.expr("Quantity * UnitPrice"))
+    )
+	print("Pergunta 12")
+	(df_final_filtered_total_value
+	.groupBy("InvoiceNo")
+		.sum("Quantity")
+		.orderBy("sum(Quantity)", ascending=False)
+		.show())
+
+def pergunta13(df):
+	
+	df_final_filtered = df.select(
+		"StockCode", "Quantity", "UnitPrice", "InvoiceDate", "Country", "CustomerID"
+			).where(
+				(~F.col('InvoiceNo').rlike("^C")) &
+				(F.col("UnitPrice") > 0) &
+				(F.col("Quantity") > 0) &
+				(F.col("CustomerID") > 0)
+			)
+	print("Pergunta 13")
+	(df_final_filtered
+	.groupBy(
+		F.col('CustomerID')
+		).count()
+		.alias("Total buy")
+		.orderBy("Total buy.count", ascending=False)
+		.show())
 
 if __name__ == "__main__":
 	sc = SparkContext()
@@ -154,10 +262,16 @@ if __name__ == "__main__":
 		          #.schema(schema_online_retail)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
 	#.print(df.show())
-	# pergunta1(df)
-	# pergunta2(df)
-	# pergunta3(df)
-	# pergunta4(df)
-	# pergunta5(df)
-	# pergunta6(df)
-	pergunta7(df)
+	df_treated = treatcsv(df)
+	# pergunta1(df_treated)
+	# pergunta2(df_treated)
+	# pergunta3(df_treated)
+	# pergunta4(df_treated)
+	# pergunta5(df_treated)
+	# pergunta6(df_treated)
+	# pergunta7(df_treated)
+	# pergunta9(df_treated)
+	# pergunta10(df_treated)
+	# pergunta11(df_treated)
+	pergunta12(df_treated)
+	# pergunta13(df_treated)
